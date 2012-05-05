f0ad42a1ef8239ea7e24c150f5d9d96b1c970ff4
florent.monjalet <florent.monjalet@29f43bec-db96-d76b-3684-d5729a2224d9>
2012-05-05 17:10:09 +0000

    Interpolation : OK.

--- trunk/src/splines.py
+++ trunk/src/splines.py
@@ -34,14 +34,6 @@ def load_foil(file):
     return(ex,ey,ix,iy)
 
 
-def plot_airfoil(ex, ey, ix, iy, file_name):
-    """ Plots an airfoil (or any pair of curves) """
-    mp.clf()
-    mp.plot(ex, ey, 'ro')
-    mp.plot(ix, iy, 'go')
-    mp.savefig(file_name)
-    mp.clf()
-
 
 # -------------------------------------------------- #
 # Cubic Splines :                                    #
@@ -90,13 +82,11 @@ def cubic_spline_interpolation_calc(X,Y,Ypp,x):
             return A*Y[i] + B*Y[i+1] + C*Ypp[i] + D*Ypp[i+1]
 
     if x >= X[N-1]:
-        return X[N-1]
+        return Y[N-1]
     else:
-        return X[0]
+        return Y[0]
     
 
-
-# A terme sera fait avec un lambda
 def cubic_spline_interpolation(X,Y):
     """ Returns a f(x) function that interpolates the cloud of points X, Y using cubic Splines """
 
@@ -112,12 +102,53 @@ def cubic_spline_interpolation(X,Y):
     
 # ------------------- Tests ------------------------ #
 
+
+def plot_airfoil(ex, ey, ix, iy, file_name):
+    """ Plots an airfoil (or any pair of curves) """
+    mp.clf()
+    mp.plot(ex, ey, 'ro')
+    mp.plot(ix, iy, 'go')
+    mp.savefig(file_name)
+    mp.clf()
+
+
+def plot_points_and_interpolation(ex, ey, ix, iy, file_name):
+    """ Plots an airfoil (or any pair of curves) and its cubic splines interpolation """
+    
+    ext_interp_fun = cubic_spline_interpolation(ex,ey)
+    int_interp_fun = cubic_spline_interpolation(ix,iy)
+    
+    nb_of_points = 1000.
+    min_interval = min(ex)
+    max_interval = max(ex)
+    pas = (max_interval - min_interval) / nb_of_points
+
+    x = np.arange(min_interval, max_interval, pas)
+
+    ext_airfoil_interpolation = np.zeros(nb_of_points)
+    int_airfoil_interpolation = np.zeros(nb_of_points)
+
+    for i in np.arange(0, nb_of_points):
+        ext_airfoil_interpolation[i] = ext_interp_fun(x[i])
+        int_airfoil_interpolation[i] = int_interp_fun(x[i])
+
+    mp.clf()
+    mp.plot(x, ext_airfoil_interpolation, 'r')
+    mp.plot(x, int_airfoil_interpolation, 'g')
+    mp.plot(ex, ey, 'r+')
+    mp.plot(ix, iy, 'g+')
+    mp.savefig("interpolation_of_the_airfoil.png")
+    mp.clf()
+
+    
+
 (ex,ey,ix,iy) = load_foil("fx63145.dat")
 plot_airfoil(ex, ey, ix, iy, "airfoil.png")
 
-ext_airfoil_interpolation = cubic_spline_interpolation(ex,ey)
-int_airfoil_interpolation = cubic_spline_interpolation(ex,ey)
+output_image_name = "interpolation_of_the_airfoil.png"
 
-
+print "Saving the results of the interpolation in", output_image_name, "..."
+plot_points_and_interpolation(ex, ey, ix, iy, output_image_name)
+print "Result written.\n"
 
 # -------------------------------------------------- #
