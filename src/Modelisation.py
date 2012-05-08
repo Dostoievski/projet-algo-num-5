a769493017f1d62d0ed7f9418f02b578f6d3388c
rianarasoldier@gmail.com <rianarasoldier@gmail.com@29f43bec-db96-d76b-3684-d5729a2224d9>
2012-05-08 22:29:09 +0000

    maj modelisation.py, formules physique ok. Mais map_pressure pas encore beau

--- src/Modelisation.py
+++ src/Modelisation.py
@@ -53,66 +53,41 @@ print deriv(z)(1)
 # EXTRADOS :                                     #
 #------------------------------------------------#
   
-def f_lambda_e(f, l):
+def f_lambda_ext(f, l):
     hmax = max(ey)
-    return 3*l*hmax + (1-l)*f(x)
+    return lambda(x):3*l*hmax + (1-l)*f(x)
 
-# def f_length_e(f_d): #returns a list (result, error)
-#     g = lambda(x): np.sqrt(1+f_d(x)**2)
-#     t = sp.quadrature(g, 0, max(ex), args=(), tol=1.5e-8, maxiter=50)
-#     return t[0]
+def f_lambda_ext_d(f, l):
+    return lambda(x):(1-l)*f(x)
 
-def P_e(Ps,r,f, l):
-    "calculates Pressure"
+def f_length_ext(f_d):
+    g = lambda(x): np.sqrt(1+f_d(x)**2)
+    t = it.simpson(g, 0, max(ex), 50)
+    return t
 
-    "Derivation calculation"
-    f_d = deriv(f)
-
-    "Length calculation"
-    #g = np.sqrt(1+f_d**2)#Probleme de dérivation...
-    calc = sp.quadrature(np.sqrt(1+f_d**2), 0, max(ex), args=(), tol=1.5e-8, maxiter=50) 
-
-    "Speed calculation"
-    v = calc/1
-    
+def pressure_ext(Ps,r,f_length_e):
+    v = f_length_e/1
     return Ps + r*v**2
-
-# def P_e(Ps,r,f_length_e):
-#    v = f_length_e/1
-#    return Ps + r*v**2
 
 #------------------------------------------------#
 # INTRADOS :                                     #
 #------------------------------------------------#
   
-def f_lambda_i(f, l):
+def f_lambda_int(f, l):
     hmin = min(iy)
     return lambda(x):3*l*hmin + (1-l)*f(x)
 
-def f_length_i(f_d): #returns a list (result, error)
+def f_lambda_int_d(f, l):
+    return lambda(x):(1-l)*f(x)
+
+def f_length_int(f_d):
     g = lambda(x): np.sqrt(1+f_d(x)**2)
-    t = sp.quadrature(g, 0, max(ix), args=(), tol=1.5e-8, maxiter=50)
-    return t[0]
-
+    t = it.simpson(g, 0, max(ix), 50)
+    return t
   
-# def P_i(Ps,r,f_length_i):
-#     v = f_length_i/1
-#     return Ps + r*v**2
-
- # def P_i(Ps, r, f_d):
- #     g = lambda(x): np.sqrt(1+f_d(x)**2)
- #     calc = sp.quadrature(g, 0, max(ix), args=(), tol=1.5e-8, maxiter=50)
- #     v = f_length_i[0]/1
- #     return Ps + r*v**2
-
-# def P_i(Ps, r, f):
-#     f_l = lambda(x): 3*l*hmax + (1-l)*f(x)
-#     f_l_d = lambda(x): derivative(f_l, x, 0.0001)
-
-#     g = lambda(x): np.sqrt(1+f_d_l(x)**2)
-#     f_length = sp.quadrature(g, 0, max(ix), args=(), tol=1.5e-8, maxiter=50)
-    
-
+def pressure_int(Ps,r,f_length_i):
+    v = f_length_i/1
+    return Ps + r*v**2
 
 
 #------------------------------------------------#
@@ -133,20 +108,32 @@ def airflow_model(f_int, f_ext):
     nbLambda = len(lambdaTab)
     
     for i in range(nbLambda):
-        g = f_lambda_i(f_int, lambdaTab[i])
+        g = f_lambda_int(f_int, lambdaTab[i])
         f_lambda_values_int = curve(g,len(t),t)
         mp.plot(t,f_lambda_values_int,linewidth=1.0)
     for j in range(nbLambda):
-        h = f_lambda_e(f_ext, lambdaTab[j])
+        h = f_lambda_ext(f_ext, lambdaTab[j])
         f_lambda_values_ext = curve(h,len(t),t)
         mp.plot(t,f_lambda_values_ext,linewidth=1.0)
 
     mp.axis([min(ex), max(max(ex),max(ix)), 3*min(iy), 3*max(ey)])
     mp.title('Laminar airflow of the fx63145')
     mp.savefig("test.png")
-
+    mp.clf()
     return 1
 
+def matrix_map_pressure(f_d, Ps, r, precision):
+    lambdaTab = np.arange(0.0,1.0,precision)
+    nbLambda = len(lambdaTab)
+    M = np.zeros([nbLambda,nbLambda])
+    lambda_tmp = 0
+    for i in np.arange(0,nbLambda,1):
+        for j in np.arange(0,nbLambda,1):
+            f_lambda = f_lambda_ext(f_d, lambdaTab[i])
+            f_length = f_length_ext(f_lambda)
+            M[i,j] = pressure_ext(Ps, r, f_length)
+    return M
+            
 def matrix_to_png(matrix, name):
     """ Convertit une matrice matrix en une image png """
     mp.imshow(matrix, cmap='hot', interpolation='bilinear')
@@ -155,35 +142,54 @@ def matrix_to_png(matrix, name):
 #------------------------------------------------#
 # TESTS :                                        #
 #------------------------------------------------#
- 
 
-#fe_a = f_lambda_e(ext_interp_fun, 0.1)
-#fe_b = f_lambda_e(ext_interp_fun, 1.0)
+print " \n ## With simpsons integrals ### \n"
+print " \n ########### EXTRADOS ######### \n "
 
-# print f_length_e(derivative(fe_a))
-# print f_length_e(derivative(fe_b))
-print P_e(1, 1, ext_interp_fun, 0.1)
+fe_a = f_lambda_ext(ext_interp_fun_d, 0.1)
+fe_b = f_lambda_ext(ext_interp_fun_d, 1.0)
 
-# print f_lambda_i(int_interp_fun, 0.1)
-# print f_length_i(int_interp_fun_d, 0.1)
-#print P_i(1, 1, f_length_i)
+print "Calculation of f_length_a for f_lambda_a with lambda = 0.1"
+f_length_ext_a = f_length_ext(fe_a)
+print f_length_ext_a
+
+print "Calculation of f_length_b for f_lambda_b with lambda = 1.0"
+f_length_ext_b = f_length_ext(fe_b)
+print f_length_ext_b
+
+print "\nCalculation of pressure_int for f_lambda_a with lambda = 0.1"
+print pressure_ext(1, 1, f_length_ext_a)
+
+print "Calculation of pressure_int for f_lambda_b with lambda = 1.0"
+print pressure_ext(1, 1, f_length_ext_b)
+
+
+print " \n ########### INTRADOS ######### \n "
+fi_a = f_lambda_int(int_interp_fun_d, 0.1)
+fi_b = f_lambda_int(int_interp_fun_d, 1.0)
+
+print "Calculation of f_length_a for f_lambda_a with lambda = 0.1"
+f_length_int_a = f_length_int(fi_a)
+print f_length_int_a
+
+print "Calculation of f_length_b for f_lambda_b with lambda = 1.0"
+f_length_int_b = f_length_int(fi_b)
+print f_length_int_b
+
+print "\nCalculation of pressure_int for f_lambda_a with lambda = 0.1"
+print pressure_int(1, 1, f_length_int_a)
+
+print "Calculation of pressure_int for f_lambda_b with lambda = 1.0"
+print pressure_int(1, 1, f_length_int_b)
 
 airflow_model(int_interp_fun, ext_interp_fun)
 
-
-import numpy as np
-# import scipy.integrate as sp
-# import splines as spl
-import pylab as mp
-
-# h = lambda(x): x
-# g = sp.quadrature(h, 0, 3, args=(), tol=1.5e-8, maxiter=50)
-# print g[0]
 
 def matrix_to_png(matrix, name):
     """ Convertit une matrice matrix en une image png """
     mp.imshow(matrix, cmap='hot', interpolation='bilinear')
     mp.savefig(name)
+    mp.clf()
     
 def Matrix_square(size):
     M = np.zeros([size,size])
@@ -204,4 +210,7 @@ def Matrix_square(size):
     return M
 
 
-print matrix_to_png(Matrix_square(20), "toto.png")
+print matrix_to_png(Matrix_square(20), "test_conversion.png")
+
+M = matrix_map_pressure(ext_interp_fun_d, 1, 1, 0.1)
+print matrix_to_png(M, "Map_pressure.png")
