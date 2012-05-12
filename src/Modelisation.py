fcc65874c06e915edfce84b8193933bcaad149b3
rianarasoldier@gmail.com <rianarasoldier@gmail.com@29f43bec-db96-d76b-3684-d5729a2224d9>
2012-05-12 12:52:44 +0000

    ajout map pressure pour methodes simspson et rectangle droite

--- src/Modelisation.py
+++ src/Modelisation.py
@@ -4,23 +4,6 @@ import splines as spl
 import pylab as mp
 import integration as it
 
-#--------Five ways to integrate a function--------#
-
-#### 1) quadrature(func, a, b, args=(), tol=1.5e-8, maxiter=50)
-#Integrate func from a to b using Gaussian quadrature with absolute tolerance tol. args is extra arguments to pass to func, and maxiter is the maximum number of iterations. Returns (val, err), where val is the Gaussian quadrature approximation, and err is the difference between the last two estimates of the integral.
-
-#### 2) romberg(function, a, b, tol=1.48e-8, show=0, divmax=10)
-#Romberg integration of a callable function or method.
-
-#### 3) dblquad(func, a, b, gfun, hfun, args=(), epsabs=1.5e-8, epsrel=1.5e-8)
-#Compute the double (definite) integral of func(y,x,*args) from x=a..b and y=gfun(x)..hfun(x). Returns (val, err).
-
-#### 4) tplquad(func, a, b, gfun, hfun, qfun, rfun, args=(), epsabs=1.5e-8, epsrel=1.5e-8)
-#Compute the triple (definite) integral of func(z,y,x,*args) from x=a..b, y=gfun(x)..hfun(x), and z=qfun(x,y)..rfun(x,y). Returns (val, err).
-
-#### 5) quad(func, a, b, args=(), full_output=0, epsabs=1.49e-8, epsrel=1.49e-8, limit=50, points=None, weight=None, wvar=None, wopts=None, maxp1=50, limlst=50)
-#Computes an integral using a technique from the Fortan library QUADPACK. The function is integrated from a to b. Run scipy.integrate.quad_explain() for more information on the more esoteric inputs and outputs.
-
 (ex,ey,ix,iy) = spl.load_foil("fx63145.dat")
 
 #-------------------- Function ------------------#
@@ -87,6 +70,8 @@ def airflow_model(f_int, f_ext):
         mp.plot(t,f_lambda_values_ext,linewidth=1.0, color='#0000ff')
 
     mp.axis([min(ex), max(max(ex),max(ix)), 3*min(iy), 3*max(ey)])
+    mp.xlabel('x')
+    mp.ylabel('y')
     mp.title('Laminar airflow of the fx63145')
     mp.savefig("airflows.png")
     mp.clf()
@@ -128,6 +113,8 @@ def curves_pressure_representation(f_int, f_int_d, f_ext,  f_ext_d, integration_
         mp.plot(t,f_lambda_values_ext,linewidth=5.0, color=(0,0,color))
 
     mp.axis([min(ex), max(max(ex),max(ix)), 3*min(iy), 3*max(ey)])
+    mp.xlabel('x')
+    mp.ylabel('y')
     mp.title('Map pressure of the fx63145 (The lighter corresponds to high pressure)')
     mp.savefig("map_pressure_test.png")
     mp.clf()
@@ -244,6 +231,7 @@ print pressure_on_curve(Ps, length_ext_b)
 airflow_model(int_interp_fun, ext_interp_fun)
 
 curves_pressure_representation(int_interp_fun, int_interp_fun_d, ext_interp_fun, ext_interp_fun_d, it.simpson, 0, 0.1, 50)
+curves_pressure_representation(int_interp_fun, int_interp_fun_d, ext_interp_fun, ext_interp_fun_d, it.rectangle_method_right, 0, 0.1, 50)
 
 Mat = plot_pressures_around_airfoil_mat("fx63145.dat", 10)
 matrix_to_png(Mat, "test_pressure.png")
